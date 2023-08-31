6c410466b0910cca9c9dd7c6d80cbd2fb4cb7be4
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-31 19:03:51 +0600

    backend connect

--- backend/api/upload-font.php
+++ backend/api/upload-font.php
@@ -1,5 +1,17 @@
 <?php 
-    require 'vendor/autoload.php';
+    // require 'vendor/autoload.php';
     header("Access-Control-Allow-Origin: *");
-    echo "File started uploading";
-?>
+//     echo "File started uploading";
+//     $uploadDirectory = 'uploads/';
+
+// if (!is_dir($uploadDirectory)) {
+//     mkdir($uploadDirectory, 0777, true);
+// }
+
+// $fontFile = $_FILES['file'];
+// echo $fontFile;
+    if(isset($_POST)){
+
+$data = json_decode(file_get_contents("php://input"));
+echo $data;
+    }
--- src/components/fileUploaderForm/FileUploaderForm.jsx
+++ src/components/fileUploaderForm/FileUploaderForm.jsx
@@ -4,20 +4,20 @@ import { BsCloudUpload } from "react-icons/bs";
 
 const FileUploaderForm = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [warningMessage, setWarningMessage] = useState('');
+  const [warningMessage, setWarningMessage] = useState("");
   const [fontInfo, setFontInfo] = useState(null);
 
   const onDrop = (acceptedFiles) => {
-     // Filter out files with the wrong extension (non-TTF files)
-     const ttfFiles = acceptedFiles.filter(file => file.name.endsWith('.ttf'));
+    // Filter out files with the wrong extension (non-TTF files)
+    const ttfFiles = acceptedFiles.filter((file) => file.name.endsWith(".ttf"));
 
-     if (ttfFiles.length === 0) {
-       // No valid TTF files were selected
-       setWarningMessage("Please select a valid TTF file.");
-     } else {
-       setSelectedFiles(ttfFiles);
-       setWarningMessage('');
-     }
+    if (ttfFiles.length === 0) {
+      // No valid TTF files were selected
+      setWarningMessage("Please select a valid TTF file.");
+    } else {
+      setSelectedFiles(ttfFiles);
+      setWarningMessage("");
+    }
   };
 
   useEffect(() => {
@@ -26,12 +26,15 @@ const FileUploaderForm = () => {
       // Fetch font info from PHP
       fetch('http://localhost/IXP-task1/font-uploader-and-grouping/backend/api/upload-font.php', {
         method: 'POST',
-        body: new FormData(document.querySelector('form')),
+        headers:{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({ "num1": 1, "num2": 2}),
       })
         .then((response) => response.json())
         .then((data) => {
-          if (data.fontInfo) {
-            setFontInfo(data.fontInfo);
+          if (data) {
+            setFontInfo(data);
           }
         })
         .catch((error) => {
@@ -41,17 +44,25 @@ const FileUploaderForm = () => {
   }, [selectedFiles]);
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: ".ttf", 
+    accept: ".ttf",
   });
-  console.log(fontInfo)
+  console.log(selectedFiles);
   return (
     <div>
-      <form  {...getRootProps()} className="w-[70%] mx-auto lg:w-auto lg:px-56 py-28 rounded-md border-dashed border-2 bg-gray-50 font-serif flex flex-col items-center gap-2 cursor-pointer">
+      <form
+        {...getRootProps()}
+        className="w-[70%] mx-auto lg:w-auto lg:px-56 py-28 rounded-md border-dashed border-2 bg-gray-50 font-serif flex flex-col items-center gap-2 cursor-pointer"
+      >
         <input {...getInputProps()} />
         <BsCloudUpload className="w-7 h-7 lg:w-[35px] lg:h-[35px] text-gray-400"></BsCloudUpload>
-        <p className="text-gray-500 text-base"><span className="font-semibold text-gray-600">Click to upload</span> or drag and drop</p>
+        <p className="text-gray-500 text-base">
+          <span className="font-semibold text-gray-600">Click to upload</span>{" "}
+          or drag and drop
+        </p>
         <p className="text-gray-500 text-sm">Only TTF file allowed</p>
-        <p className="text-xs font-normal text-red-500 animate-pulse">{warningMessage}</p>
+        <p className="text-xs font-normal text-red-500 animate-pulse">
+          {warningMessage}
+        </p>
       </form>
     </div>
   );
